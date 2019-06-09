b3aad6f97972287f737b7333d75709a92aa35699
TAAPArthur <taaparthur@gmail.com>
2019-06-09 15:09:10 -0500

    Updated package list
    Fixed security hole with linking to systemd files
    Added note to configure sudo so restore script works when setting up a new machine
    Added virtualization group

--- restore.sh
+++ restore.sh
@@ -10,10 +10,17 @@ if grep -q arthur /etc/passwd;then
     usermod -G sys,disk,lp,wheel,rfkill,video,optical,storage,scanner,input,users arthur
 else
     useradd -m -G sys,disk,lp,wheel,rfkill,video,optical,storage,scanner,input,users arthur
+    passwd -d arthur
 fi
+echo "Remember to use visudo to allow wheel to use sudo"
 
-sudo pacman -S git jq --needed
+sudo pacman -S git vim jq --needed
 
-su arthur -c "cd ~;[[ -d SystemConfig ]] || git clone git@github.com:TAAPArthur/SystemConfig.git"
-su arthur -c "[[ -d /tmp/system-manager ]] || git clone git@github.com:TAAPArthur/system-manager.git; cd /tmp/system-manager; makepkg -sic"
-su arthur -c "system-manager run-scripts"
+su arthur -c "cd ~;[[ -d SystemConfig ]] || git clone https://github.com/TAAPArthur/SystemConfig.git"
+cd /tmp
+su -p arthur -c "[[ -d /tmp/system-manager ]] || (git clone https://github.com/TAAPArthur/system-manager.git && cd /tmp/system-manager && makepkg -sc;)"
+pacman -U /tmp/system-manager/*.tar.xz --needed
+
+su arthur -P -c "system-manager init"
+su arthur --login -P -c "system-manager install"
+su arthur -P -c "system-manager link"
