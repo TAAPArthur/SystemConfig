1625b73c9e7c47e2b4ebd13f24a9b9756a3e1c23
Arthur Williams <taaparthur@gmail.com>
2019-10-31 22:46:10 -0700

    Improved parsing of weather
    We no longer get those indecipherable text when failing to parse
    weather.

--- bin/weather.sh
+++ bin/weather.sh
@@ -2,33 +2,29 @@
 
 main () {
 
-file="/tmp/.weather"
+    file="/tmp/.weather"
 
-if [[ -f "$file" && $(date -r "$file" +%s) > $(date -d 'now - 1 hour' +%s) ]]; then
-    cat $file
-    exit 0
-fi
+    if [[ -f "$file" && ($(date -r "$file" +%s) > $(date -d 'now - 1 hour' +%s)) ]]; then
+        cat $file
+        exit 0
+    fi
 
-if ! ping -q -c 1 -W 1 google.com &>/dev/null; then
-    echo " ... "
-    exit 1
-fi
-
-
-tempC=$(curl -s wttr.in?m | sed -E -n '3,7{s/\d27\[[0-9;]*m//g;s/^.* (-?[0-9]+)..\+?(-?[0-9]+) (.[CF]).*$/\1-\2\3/p'})
-tempF=$(curl -s wttr.in?u | sed -E -n '3,7{s/\d27\[[0-9;]*m//g;s/^.* (-?[0-9]+)..\+?(-?[0-9]+) (.[CF]).*$/\1-\2\3/p'})
-if [ "$?" -ne 0 ] || [ -z "$tempC" ]; then
-    tempC=$(curl -s wttr.in?m | sed -E -n '3,7{s/\d27\[[0-9;]*m//g;s/^.* (-?[0-9]+) (.[CF]).*$/\1\2/p'})
-    tempF=$(curl -s wttr.in?u | sed -E -n '3,7{s/\d27\[[0-9;]*m//g;s/^.* (-?[0-9]+) (.[CF]).*$/\1\2/p'})
-fi
-if [ "$?" -ne 0 ] || [ -z "$tempC" ]; then
-    echo $(cat $file)'*'
-else
-    output=$(echo "$tempC;$tempF" |tr -d " ")
-    echo "$output" >$file
-    echo "$output"
-fi
+    if ! ping -q -c 1 -W 1 google.com &>/dev/null; then
+        touch $file
+        echo $(cat $file)'*'
+        exit 1
+    fi
+    tempC=$(curl -s 'wttr.in?m&format=%t&lang=uk')
+    tempF=$(curl -s 'wttr.in?u&format=%t&lang=uk')
+    if [ "$?" -ne 0 ] || [ -z "$tempC" ]; then
+        touch $file
+        echo $(cat $file)'*'
+    else
+        output=$(echo "$tempC;$tempF"|sed -r 's/[^CF0-9; ]//g' )
+        echo "$output" >$file
+        echo "$output"
+    fi
 
 }
 
-main &
+main
