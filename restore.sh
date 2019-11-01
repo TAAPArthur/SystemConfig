d17663e088946f70c83fb664f07d849ee5915aab
Arthur Williams <taaparthur@gmail.com>
2019-10-31 23:01:34 -0700

    Updated install script

--- restore.sh
+++ restore.sh
@@ -1,7 +1,9 @@
 #~/bin/bash
 set -xe
 
-if [[ "$1" == "update" || $1 == "-u" ]]; then
+user=${user:-arthur}
+
+if [[ "$1" == "update" || "$1" == "-u" ]]; then
     cd /tmp
     [[ -d SystemConfig ]] && (cd SystemConfig && git pull) || git clone https://github.com/TAAPArthur/SystemConfig.git
     cd SystemConfig
@@ -10,28 +12,28 @@ if [[ "$1" == "update" || $1 == "-u" ]]; then
     exit 0
 fi
 
-
-if grep -q nonfree /etc/passwd;then
-    usermod -G games,users,input,video nonfree
-else
-    useradd -m -G games,users,input,video nonfree
+if [[ "$1" != "-s" ]]; then
+	if grep -q nonfree /etc/passwd;then
+	    usermod -G games,users,input,video nonfree
+	else
+	    useradd -m -G games,users,input,video nonfree
+	fi
 fi
-if grep -q arthur /etc/passwd;then
-    usermod -G sys,disk,lp,wheel,rfkill,video,optical,storage,scanner,input,users arthur
+if grep -q $user /etc/passwd;then
+    usermod -G sys,disk,lp,wheel,rfkill,video,optical,storage,scanner,input,users $user
 else
-    useradd -m -G sys,disk,lp,wheel,rfkill,video,optical,storage,scanner,input,users arthur
-    passwd -d arthur
+    useradd -m -G sys,disk,lp,wheel,rfkill,video,optical,storage,scanner,input,users $user
+    passwd -d $user
 fi
 echo "Remember to use visudo to allow wheel to use sudo"
 
 sudo pacman -S git vim jq --needed
 
-su arthur -c "cd ~;[[ -d SystemConfig ]] && (cd SystemConfig && git pull) || git clone https://github.com/TAAPArthur/SystemConfig.git"
+su $user -c "cd ~;[[ -d SystemConfig ]] && (cd SystemConfig && git pull) || git clone https://github.com/TAAPArthur/SystemConfig.git"
 cd /tmp
-su -p arthur -c "[[ -d /tmp/system-manager ]] || (git clone https://github.com/TAAPArthur/system-manager.git && cd /tmp/system-manager && makepkg -sc;)"
-pacman -U /tmp/system-manager/*.tar.xz --needed
+pacman -Qs system-manager >>/dev/null || su -p $user -c "[[ -d /tmp/system-manager ]] || (git clone https://github.com/TAAPArthur/system-manager.git && cd /tmp/system-manager && makepkg -sc;)" && pacman -U /tmp/system-manager/*.tar.xz --needed
 
-su arthur -P -c "system-manager init"
-su arthur -P -c "system-manager link-normal"
-su arthur --login -P -c "system-manager install"
-su arthur -P -c "system-manager link-root"
+su $user -P -c "system-manager init"
+su $user -P -c "system-manager link-normal"
+su $user --login -P -c "system-manager install"
+su $user -P -c "system-manager link-root"
