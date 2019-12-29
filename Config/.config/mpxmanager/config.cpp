64c1bbc57256a108441d1c611b8d383aa9676adf
Arthur Williams <taaparthur@gmail.com>
2019-12-29 08:32:33 -0800

    Updated and moved mpxmanager

--- Config/.config/mpxmanager/config.cpp
+++ Config/.config/mpxmanager/config.cpp
@@ -7,38 +7,42 @@
 #include <mpxmanager/Extensions/window-clone.h>
 #include <mpxmanager/Extensions/xmousecontrol.h>
 #include <mpxmanager/bindings.h>
+#include <mpxmanager/chain.h>
 #include <mpxmanager/debug.h>
 #include <mpxmanager/device-grab.h>
 #include <mpxmanager/extra-rules.h>
 #include <mpxmanager/functions.h>
 #include <mpxmanager/globals.h>
+#include <mpxmanager/layouts.h>
 #include <mpxmanager/logger.h>
 #include <mpxmanager/settings.h>
 #include <mpxmanager/system.h>
 #include <mpxmanager/test-functions.h>
+#include <mpxmanager/window-properties.h>
 #include <mpxmanager/windows.h>
 #include <mpxmanager/wm-rules.h>
-#include <mpxmanager/chain.h>
 #include <mpxmanager/wmfunctions.h>
 
 
 Binding customBindings[] = {
-    {0, XK_F6, {raiseOrRun, "arandr"} },
     {Mod4Mask, XK_F6, {raiseOrRun, "arandr"} },
+    {0, XF86XK_Display, {spawn, "autorandr -c"} },
+    {ShiftMask, XF86XK_Display, {spawn, "xsane-xrandr -i configure"} },
+    {Mod4Mask, XF86XK_Display, {spawn, "arandr"} },
 
-    {Mod4Mask, XF86XK_MonBrightnessUp, {spawn, "~/Documents/Keyboard_Commands/brightness .1"} },
-    {Mod4Mask | ShiftMask, XF86XK_MonBrightnessUp, {spawn, "~/Documents/Keyboard_Commands/brightness -a 1"} },
-    {Mod4Mask, XF86XK_MonBrightnessDown, {spawn, "~/Documents/Keyboard_Commands/brightness -.1"} },
-    {Mod4Mask | ShiftMask, XF86XK_MonBrightnessDown, {spawn, "~/Documents/Keyboard_Commands/brightness -a 1"} },
+    {0, XF86XK_MonBrightnessUp, {spawn, "brightnessctl s +1"} },
+    {0, XF86XK_MonBrightnessDown, {spawn, "brightnessctl s -1"} },
+    {Mod4Mask, XF86XK_MonBrightnessUp, {spawn, "brightness +.1"} },
+    {Mod4Mask | ShiftMask, XF86XK_MonBrightnessUp, {spawn, "brightness"} },
+    {Mod4Mask, XF86XK_MonBrightnessDown, {spawn, "brightness -.1"} },
+    {Mod4Mask | ShiftMask, XF86XK_MonBrightnessDown, {spawn, "brightness"} },
     {Mod4Mask | ControlMask, XF86XK_MonBrightnessDown, {spawn, "xrandr-invert-colors"} },
     {Mod4Mask | ControlMask, XF86XK_MonBrightnessUp, {spawn, "xrandr-invert-colors"} },
     {0, XF86XK_Tools, {spawn, "xrandr-invert-colors"} },
 
     {Mod4Mask, XK_v, {spawn, "clip-history select -w $_WIN_ID && xsel --clipboard | xvkbd -window $_WIN_ID -file - 2>/dev/null"} },
 
-    {Mod4Mask, XK_p, {spawn, "dmenu_run_history"} },
-    {Mod4Mask | ControlMask, XK_p, {spawn, "dmenu_extended_run"} },
-    {Mod4Mask | ControlMask, XK_Menu, {raiseOrRun, "blueman-manager"} },
+    {Mod4Mask, XK_p, {spawn, "dmenu_run"} },
 
     {0, XK_Print, {spawn, "maim -s | xclip -selection clipboard -t image/png"} },
     {ShiftMask, XK_Print, {spawn, "maim -s /tmp/$(date +%s).png"} },
@@ -47,8 +51,8 @@ Binding customBindings[] = {
 
     {0, XF86XK_Eject, {spawn, "dmenu-pycalc"} },
     {0, XF86XK_Calculator, {spawn, "dmenu-pycalc"} },
-    {Mod4Mask, XK_Delete, +[] {raiseOrRun("gt5", "$TERMINAL -e 'gt5 /' -T gt5", 0);} },
-    {Mod4Mask| ShiftMask, XK_Delete, +[] {raiseOrRun("gt5", "$TERMINAL -e 'gt5 ~' -T gt5", 0);} },
+    {Mod4Mask, XK_Delete, +[] {raiseOrRun("gt5", "/usr/bin/$TERMINAL -r gt5 -e 'gt5 /' -T gt5", MATCHES_ROLE);} },
+    {Mod4Mask| ShiftMask, XK_Delete, +[] {raiseOrRun("gt5", "/usr/bin/$TERMINAL -r gt5 -e 'gt5 ~' -T gt5", MATCHES_ROLE);} },
 
     {ControlMask | Mod1Mask, XK_Delete, {raiseOrRun, "notify-send -t 20000  CPU \"$(ps -Ao pid,pcpu,pmem,comm --sort=-pcpu --cols=27 | head -n 6)\" && notify-send -t 20000  MEM \"$(ps -Ao pid,pcpu,pmem,comm --sort=-pmem --cols=27 | head -n 6)\" "},{ .noKeyRepeat = 1}},
 
@@ -56,9 +60,17 @@ Binding customBindings[] = {
     {ControlMask | Mod1Mask, XK_Escape, {spawn, "systemctl suspend"}, {.noKeyRepeat = 1} },
     {Mod4Mask | ControlMask | Mod1Mask, XK_Escape, {spawn, "notify-send 'System will shutdown in 1 min'; shutdown 1"}, {.noKeyRepeat = 1} },
 
+
+    {0, XF86XK_Bluetooth, {spawn, "rfkill unblock bluetooth"} },
+    {ShiftMask, XF86XK_Bluetooth, {spawn, "rfkill block bluetooth"} },
+
     {0, XF86XK_AudioLowerVolume, {spawn, "pamixer -d 1"} },
     {0, XF86XK_AudioRaiseVolume, {spawn, "pamixer -i 1"} },
     {0, XF86XK_AudioMute, {spawn, "pamixer -t"} },
+    {ShiftMask, XF86XK_AudioLowerVolume, {spawn, "pamixer --default-source -d 1"} },
+    {ShiftMask, XF86XK_AudioRaiseVolume, {spawn, "pamixer --default-source -i 1"} },
+    {ShiftMask, XF86XK_AudioMute, {spawn, "pamixer -t"} },
+    {0, XF86XK_AudioMicMute, {spawn, "pamixer --default-source -t"} },
     {Mod4Mask, XF86XK_AudioLowerVolume, {spawn, "omnivolctrl -1%"} },
     {Mod4Mask, XF86XK_AudioRaiseVolume, {spawn, "omnivolctrl +1%"} },
     {Mod4Mask, XF86XK_AudioMute, {spawn, "omnivolctrl sink-input-mute toggle"} },
@@ -67,21 +79,22 @@ Binding customBindings[] = {
 
     {Mod4Mask, XK_Menu, {raiseOrRun, "pavucontrol"} },
 
-    {Mod4Mask, XK_w, {raiseOrRun, "$BROWSER"} },
+    {Mod4Mask, XK_w, +[]{return raiseOrRun("browser", "$BROWSER", MATCHES_ROLE);} },
     {Mod4Mask | ShiftMask, XK_w, {spawn, "$BROWSER_ALT"} },
-    {Mod4Mask, XK_f, {raiseOrRun, "firefox"} },
-    {Mod4Mask | ShiftMask, XK_f, {spawn, "firefox --new-window"} },
 
     {Mod4Mask | ControlMask, XK_v, {raiseOrRun, "vlc"} },
-    {Mod4Mask, XK_m, {raiseOrRun, "virtualbox"} },
     {Mod4Mask | ControlMask | Mod1Mask, XK_v, {raiseOrRun, "pitivi"} },
     {Mod4Mask | ControlMask | Mod1Mask, XK_a, {raiseOrRun, "audacity"} },
-    {Mod4Mask | ControlMask, XK_g, {raiseOrRun, "gummi"} },
     {Mod4Mask | ControlMask, XK_d, {raiseOrRun, "dolphin-emu"} },
+    {Mod4Mask, XK_g, {spawn, "stream-video"} },
+    {Mod4Mask | ShiftMask, XK_g, {spawn, "stream-video --save"} },
     {Mod4Mask, XK_s, {raiseOrRun, "steam"} },
     {Mod4Mask, XK_a, {raiseOrRun, "xreader"} },
     {ControlMask | Mod1Mask, XK_t, {raiseOrRun, "$TERMINAL"} },
-    {ControlMask | Mod1Mask | ShiftMask, XK_t, {spawn, "$TERMINAL"} },
+
+    {0, XF86XK_AudioPlay, +[]() {toggleLayout(FULL);}},
+    {DEFAULT_MOD_MASK, XF86XK_AudioPlay, {toggleMask,FULLSCREEN_MASK}},
+    {DEFAULT_MOD_MASK | ShiftMask, XF86XK_AudioPlay, {toggleMask,ROOT_FULLSCREEN_MASK}},
 
     {Mod3Mask, XK_f, centerMouseInWindow},
     {Mod4Mask, XK_n, {cycleSlave, UP}},
@@ -118,23 +131,12 @@ void loadCustomAtoms() {
     LOG(LOG_LEVEL_INFO, "Loading steam atom %d\n", STEAM_GAME);
 }
 bool isSteamGame(WindowID win) {
-    xcb_get_property_cookie_t cookie;
-    xcb_get_property_reply_t* reply;
-    bool result = 0;
-    LOG(LOG_LEVEL_INFO, "Checking Steam %d\n", STEAM_GAME);
-    dumpAtoms(&STEAM_GAME, 1);
-    cookie = xcb_get_property(dis, 0, win, STEAM_GAME, XCB_ATOM_CARDINAL, 0, sizeof(int));
-    if((reply = xcb_get_property_reply(dis, cookie, NULL)) && xcb_get_property_value_length(reply)) {
-        result = 1;
-        LOG(LOG_LEVEL_INFO, "Found steam game\n\n");
-        free(reply);
-    }
-    return result;
+    auto reply = loadPropertyFromAtom(win, STEAM_GAME, XCB_ATOM_CARDINAL);
+    return reply?1:0;
 }
 void handleSteamGame(WindowInfo*winInfo) {
     if(isSteamGame(winInfo->getID())) {
         winInfo->addMask(FULLSCREEN_MASK );
-        std::cout<<*winInfo<<"\n";
     }
 }
 
@@ -144,36 +146,28 @@ void loadSettings() {
     SHELL = getenv("SHELL");
     spawnPipe("dzen2 -ta l -fg '#00FF00' -bg '#000000' -dock -fn 'xft:Bitstream Vera Sans Mono:size = 10:antialias = true' -h 20 -e ''");
 
-    // static Rule steamRule = {"Steam", MATCH_ANY_LITERAL, BIND(returnTrue), .passThrough = NO_PASSTHROUGH, .filterMatch = NBIND(hasMask, INPUT_MASK)} ;
-    // addToList(getEventRules(ProcessingWindow), &steamRule);
-    /*
-    static Rule steamRule = {"Steam", MATCH_ANY_LITERAL, BIND(floatWindow), .filterMatch = NBIND(hasMask, INPUT_MASK)} ;
-    addToList(getEventRules(PropertyLoad), &steamRule);
-    static Rule steamNewsRule = {"Steam - News.*", TITLE, BIND(moveWindowToWorkspace, -1)} ;
-    addToList(getEventRules(PropertyLoad), &steamNewsRule);
-    */
-
-    //static Rule floatRule = {".*(Preferences|Properties).*", MATCH_ANY_REGEX, BIND(floatWindow)} ;
-    //addToList(getEventRules(ClientMapAllow), );
-
-
-    getWorkspace(8)->setName("7Gaming");
-    getWorkspace(8)->addMask(FLOATING_MASK);
-
-    getBatchEventRules(onScreenChange).add( {spawn, "conky-install.sh;nitrogen --restore 2>/dev/null"} );
-
-    getEventRules(onXConnection).add( {loadCustomAtoms, "loadCustomAtoms"}, PREPEND_UNIQUE );
-    getEventRules(ClientMapAllow).add( USER_EVENT(handleSteamGame));
-    getEventRules(PreRegisterWindow).add(USER_EVENT(addFocusMask));
-    getEventRules(ClientMapAllow).add({+[](WindowInfo*winInfo) {if(matchesClass(winInfo, "dzen2"))winInfo->addMask(PRIMARY_MONITOR_MASK|ALWAYS_ON_BOTTOM);},"AOB dzen2"});
-    getEventRules(ClientMapAllow).add({+[](WindowInfo*winInfo) {if(winInfo->getType()==ewmh->_NET_WM_WINDOW_TYPE_NOTIFICATION)winInfo->addMask(ALWAYS_ON_TOP);},"AOT notifications"} );
-    addDieOnIntegrityCheckFailRule();
-
     loadNormalSettings();
     for(Binding& b : customBindings)
         getDeviceBindings().add(b);
     for(Chain* b : chainBindings)
         getDeviceBindings().add(b);
+
+    getWorkspace(8)->setName("7Gaming");
+    getWorkspace(8)->addMask(FLOATING_MASK);
+
+    getBatchEventRules(SCREEN_CHANGE).add( {spawn, "conky-install.sh;nitrogen --restore 2>/dev/null"} );
+
+    getEventRules(X_CONNECTION).add( {loadCustomAtoms, "loadCustomAtoms"}, PREPEND_UNIQUE );
+    getEventRules(PRE_REGISTER_WINDOW).add(USER_EVENT(addFocusMask));
+    getEventRules(CLIENT_MAP_ALLOW).add( {
+        +[](WindowInfo*winInfo){
+            if(matchesTitle(winInfo,"Steam - News"))
+                winInfo->moveToWorkspace(8);
+    },"HideSteamNews"});
+    getEventRules(CLIENT_MAP_ALLOW).add(USER_EVENT(handleSteamGame));
+    getEventRules(CLIENT_MAP_ALLOW).add({+[](WindowInfo*winInfo) {if(matchesClass(winInfo, "dzen2"))winInfo->addMask(PRIMARY_MONITOR_MASK|ALWAYS_ON_BOTTOM_MASK);},"AOB dzen2"});
+    getEventRules(CLIENT_MAP_ALLOW).add({+[](WindowInfo*winInfo) {if(winInfo->getType()==ewmh->_NET_WM_WINDOW_TYPE_NOTIFICATION)winInfo->addMask(ALWAYS_ON_TOP_MASK);},"AOT notifications"} );
+
 
     addKeepTransientsOnTopRule();
     addResumeCustomStateRules();
@@ -187,6 +181,8 @@ void loadSettings() {
     startAutoUpdatingClones();
     addDefaultXMouseControlBindings();
     addStartXMouseControlRule();
+
+    addAllDebugRules();
 }
 __attribute__((constructor)) static void _onStart() {
 }
