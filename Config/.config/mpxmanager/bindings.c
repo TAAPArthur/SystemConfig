5b3309e65d7718779479aa2a9adda7a684c3ff17
Arthur Williams <taaparthur@gmail.com>
2020-10-11 23:55:31 -0700

    Updated MPXManager config

---
+++ Config/.config/mpxmanager/bindings.c
@@ -0,0 +1,138 @@
+#include <stdlib.h>
+#include <X11/XF86keysym.h>
+#include <X11/keysym.h>
+
+#include <mpxmanager/Extensions/containers.h>
+#include <mpxmanager/Extensions/mpx.h>
+#include <mpxmanager/Extensions/session.h>
+#include <mpxmanager/Extensions/window-clone.h>
+#include <mpxmanager/bindings.h>
+#include <mpxmanager/boundfunction.h>
+#include <mpxmanager/util/debug.h>
+#include <mpxmanager/xutil/device-grab.h>
+#include <mpxmanager/Extensions/extra-rules.h>
+#include <mpxmanager/functions.h>
+#include <mpxmanager/globals.h>
+#include <mpxmanager/layouts.h>
+#include <mpxmanager/util/logger.h>
+#include <mpxmanager/settings.h>
+#include <mpxmanager/system.h>
+#include <mpxmanager/xutil/window-properties.h>
+#include <mpxmanager/windows.h>
+#include <mpxmanager/wm-rules.h>
+#include <mpxmanager/wmfunctions.h>
+#include "config.h"
+
+
+void containTablessWindows() {
+    containWindows(getWorkspace(3), (WindowFunctionArg) {matchesClass,  .arg = {.str = "vimb"}}, "vimb");
+    containWindows(getWorkspace(4), (WindowFunctionArg) {matchesClass,  .arg = {.str = "st"}}, "st");
+}
+
+Binding customBindings[] = {
+    // Freeze focus stack while Mod4Mask (super) is held
+    {0, XK_Super_L, {grabActiveKeyboard, {0}} },
+    {0, XK_Super_L, {setFocusStackFrozen, {1}} },
+    {Mod4Mask, XK_Super_L, {setFocusStackFrozen, {0}}, .flags = {.mask = XCB_INPUT_XI_EVENT_MASK_KEY_RELEASE}},
+    {Mod4Mask, XK_Super_L, {ungrabActiveKeyboard, {0}}, .flags = {.mask = XCB_INPUT_XI_EVENT_MASK_KEY_RELEASE}},
+
+    {Mod4Mask, XK_F6, {raiseOrRun,  .arg = {.str = "arandr"}} },
+    {0, XF86XK_Display, {spawn,  .arg = {.str = "autorandr -c"}} },
+    {ShiftMask, XF86XK_Display, {spawn,  .arg = {.str = "xsane-xrandr -i configure"}} },
+    {Mod4Mask, XF86XK_Display, {raiseOrRun,  .arg = {.str = "arandr"}} },
+
+    {0, XF86XK_MonBrightnessUp, {spawn,  .arg = {.str = "brightnessctl s +1"}} },
+    {0, XF86XK_MonBrightnessDown, {spawn,  .arg = {.str = "brightnessctl s 1-"}} },
+    {Mod4Mask, XF86XK_MonBrightnessUp, {spawn,  .arg = {.str = "brightness +.1"}} },
+    {Mod4Mask | ShiftMask, XF86XK_MonBrightnessUp, {spawn,  .arg = {.str = "brightness"}} },
+    {Mod4Mask, XF86XK_MonBrightnessDown, {spawn,  .arg = {.str = "brightness -.1"}} },
+    {Mod4Mask | ShiftMask, XF86XK_MonBrightnessDown, {spawn,  .arg = {.str = "brightness"}} },
+    {Mod4Mask | ControlMask, XF86XK_MonBrightnessDown, {spawn,  .arg = {.str = "xrandr-invert-colors"}} },
+    {Mod4Mask | ControlMask, XF86XK_MonBrightnessUp, {spawn,  .arg = {.str = "xrandr-invert-colors"}} },
+    {0, XF86XK_Tools, {spawn,  .arg = {.str = "xrandr-invert-colors"}} },
+
+    {Mod4Mask, XK_v, {spawn,  .arg = {.str = "clip-history select -w $_WIN_ID"}} },
+    {Mod4Mask | ShiftMask, XK_v, {spawn,  .arg = {.str = "clip-history select -w $_WIN_ID && xsel --clipboard | xvkbd -window $_WIN_ID -file - 2>/dev/null"}} },
+
+    {Mod4Mask, XK_p, {spawn,  .arg = {.str = "dmenu_run"}} },
+
+    {0, XK_Print, {spawn,  .arg = {.str = "screenshot maim -s"}} },
+    {ShiftMask, XK_Print, {spawn,  .arg = {.str = "maim -i $PICTURES_DIR/$(date +%s).png"}} },
+    {Mod4Mask, XK_Print, {spawn,  .arg = {.str = "maim -i $_WIN_ID| xclip -selection clipboard -t image/png"}} },
+    {Mod4Mask | ShiftMask, XK_Print, {spawn,  .arg = {.str = "maim -i $_WIN_ID $PICTURES_DIR/$(date +%s).png"}} },
+    {ControlMask, XK_Print, {spawn,  .arg = {.str = "screenshot maim -i $_WIN_ID"}} },
+
+    {0, XF86XK_Eject, {spawn,  .arg = {.str = "dmenu-pycalc"}} },
+    {0, XF86XK_Calculator, {spawn,  .arg = {.str = "dmenu-pycalc"}} },
+    {Mod4Mask, XK_Delete, {raiseOrRun,  .arg = {.str = "gt5"}, .arg2.str = "$TERMINAL -c gt5 -e gt5 /"} },
+    {Mod4Mask | ShiftMask, XK_Delete, {raiseOrRun,  .arg = {.str = "gt5"}, .arg2.str = "$TERMINAL -c gt5 -e gt5 ~"} },
+
+    {ControlMask | Mod1Mask, XK_Delete, {spawn,  .arg = {.str = "notify-send -t 20000  CPU \"}$(ps -Ao pid,pcpu,pmem,comm --sort=-pcpu --cols=27 | head -n 6)\" && notify-send -t 20000  MEM \"$(ps -Ao pid,pcpu,pmem,comm --sort=-pmem --cols=27 | head -n 6)\" "}}, { .noKeyRepeat = 1}},
+
+    {Mod4Mask, XK_Escape, {spawn,  .arg = {.str = "xtrlock"}}, {.noKeyRepeat = 1} },
+    {ControlMask | Mod1Mask, XK_Escape, {spawn, .arg.str = SUSPEND_CMD}, {.noKeyRepeat = 1} },
+    {Mod4Mask | ControlMask | Mod1Mask, XK_Escape, {spawn, .arg.str = SHUTDOWN_CMD}, {.noKeyRepeat = 1} },
+
+
+    {0, XF86XK_Bluetooth, {spawn,  .arg = {.str = "rfkill unblock bluetooth"}} },
+    {ShiftMask, XF86XK_Bluetooth, {spawn,  .arg = {.str = "rfkill block bluetooth"}} },
+
+    {0, XF86XK_AudioLowerVolume, {spawn,  .arg = {.str = "pamixer -d 1"}} },
+    {0, XF86XK_AudioRaiseVolume, {spawn,  .arg = {.str = "pamixer -i 1"}} },
+    {0, XF86XK_AudioMute, {spawn,  .arg = {.str = "pamixer -t"}} },
+    {ShiftMask, XF86XK_AudioLowerVolume, {spawn,  .arg = {.str = "pamixer --default-source -d 1"}} },
+    {ShiftMask, XF86XK_AudioRaiseVolume, {spawn,  .arg = {.str = "pamixer --default-source -i 1"}} },
+    {ShiftMask, XF86XK_AudioMute, {spawn,  .arg = {.str = "pamixer -t"}} },
+    {0, XF86XK_AudioMicMute, {spawn,  .arg = {.str = "pamixer --default-source -t"}} },
+    {Mod3Mask, XF86XK_AudioLowerVolume, {spawn,  .arg = {.str = "omnivolctrl clear"}} },
+    {Mod3Mask, XF86XK_AudioRaiseVolume, {spawn,  .arg = {.str = "omnivolctrl clear"}} },
+    {Mod4Mask, XF86XK_AudioLowerVolume, {spawn,  .arg = {.str = "omnivolctrl -1%"}} },
+    {Mod4Mask, XF86XK_AudioRaiseVolume, {spawn,  .arg = {.str = "omnivolctrl +1%"}} },
+    {Mod4Mask, XF86XK_AudioMute, {spawn,  .arg = {.str = "omnivolctrl sink-input-mute toggle"}} },
+    {Mod4Mask, XF86XK_AudioPrev, {spawn,  .arg = {.str = "omnivolctrl sink-inputs"}} },
+    {0, XF86XK_AudioPrev, {spawn,  .arg = {.str = "omnivolctrl sinks"}} },
+
+    {Mod4Mask, XK_Menu, {raiseOrRun,  .arg = {.str = "pavucontrol"}} },
+
+    {Mod4Mask, XK_w, {raiseOrRunRole,  .arg = {.str = "browser"}, .arg2.str = "$BROWSER"}, },
+    {Mod4Mask | ShiftMask, XK_w, {spawn,  .arg = {.str = "$BROWSER_ALT"}} },
+    {Mod4Mask | ControlMask | ShiftMask, XK_w, {spawn,  .arg = {.str = "$BROWSER"}} },
+    {Mod4Mask | ControlMask, XK_w, {spawn,  .arg = {.str = "firefox"}} },
+
+    {Mod4Mask | ControlMask, XK_d, {raiseOrRun,  .arg = {.str = "dolphin-emu"}} },
+    {Mod4Mask, XK_s, {raiseOrRun,  .arg = {.str = "$TERMINAL"}} },
+    {Mod4Mask | ShiftMask, XK_s, {spawn,  .arg = {.str = "$TERMINAL"}} },
+
+    {0, XF86XK_AudioPlay, {toggleActiveLayoutOrCycle, .arg = {.p = &FULL}}},
+    {DEFAULT_MOD_MASK, XF86XK_AudioPlay, {toggleMask, {FULLSCREEN_MASK}}, .flags = {.windowToPass = FOCUSED_WINDOW}},
+    {DEFAULT_MOD_MASK | ShiftMask, XF86XK_AudioPlay, {toggleMask, {ROOT_FULLSCREEN_MASK}}, .flags = {.windowToPass = FOCUSED_WINDOW}},
+
+    {Mod3Mask, XK_f, {centerMouseInWindow}, .flags = {.windowToPass = FOCUSED_WINDOW}},
+    /*
+    {Mod4Mask, XK_n, {cycleSlave, UP}},
+    {Mod3Mask, XK_n, {cycleSlave, UP}},
+    {Mod4Mask | ShiftMask, XK_n, {cycleSlave, DOWN}},
+    {Mod3Mask | ShiftMask, XK_n, {cycleSlave, DOWN}},
+
+    */
+    {Mod4Mask, XK_d, {startMPX}},
+    {Mod4Mask | ControlMask, XK_d, {(void(*)())saveMPXMasterInfo}},
+    {Mod4Mask | ShiftMask, XK_d, {destroyAllNonDefaultMasters}},
+
+    /*
+    {Mod4Mask, XK_x, {cloneFocusedWindow}},
+    {Mod4Mask | ShiftMask, XK_x, {killAllClones}},
+    */
+
+    {Mod4Mask, XK_z, {containTablessWindows}},
+    {Mod4Mask | ShiftMask, XK_z, {releaseAllWindows}},
+    //{Mod4Mask | Mod1Mask, XK_z, createSimpleContainer},
+    {Mod3Mask, XK_t, {toggleContainer}},
+
+    SPLIT_MASTER_BINDING(Mod4Mask | Mod1Mask, XK_d),
+
+} ;
+
+void addNormalBindings() {
+    addBindings(customBindings, LEN(customBindings));
+}
---
+++ Config/.config/mpxmanager/config.c
@@ -0,0 +1,142 @@
+#include <stdlib.h>
+#include <X11/XF86keysym.h>
+#include <X11/keysym.h>
+
+#include <mpxmanager/Extensions/compatibility-rules.h>
+#include <mpxmanager/Extensions/containers.h>
+#include <mpxmanager/Extensions/ewmh.h>
+#include <mpxmanager/Extensions/extra-rules.h>
+#include <mpxmanager/Extensions/mpx.h>
+#include <mpxmanager/Extensions/session.h>
+#include <mpxmanager/Extensions/window-clone.h>
+#include <mpxmanager/bindings.h>
+#include <mpxmanager/boundfunction.h>
+#include <mpxmanager/functions.h>
+#include <mpxmanager/globals.h>
+#include <mpxmanager/globals.h>
+#include <mpxmanager/layouts.h>
+#include <mpxmanager/settings.h>
+#include <mpxmanager/system.h>
+#include <mpxmanager/util/logger.h>
+#include <mpxmanager/windows.h>
+#include <mpxmanager/wm-rules.h>
+#include <mpxmanager/wmfunctions.h>
+#include <mpxmanager/xutil/window-properties.h>
+
+bool TOUCH_SCREEN_AS_MOUSE_NATIVE;
+bool NO_GESTURES;
+
+xcb_atom_t STEAM_GAME;
+void loadCustomAtoms() {
+    STEAM_GAME = getAtom("STEAM_GAME");
+    INFO("Loading steam atom %d\n", STEAM_GAME);
+}
+bool isSteamGame(WindowID win) {
+    return getWindowPropertyValueInt(win, STEAM_GAME, XCB_ATOM_CARDINAL);
+}
+void handleSteamGame(WindowInfo* winInfo) {
+    if(isSteamGame(winInfo->id)) {
+        addMask(winInfo, FULLSCREEN_MASK);
+    }
+}
+
+void configureWindowsByName(WindowInfo* winInfo) {
+    if(matchesTitle(winInfo, "mystatusbar")) {
+        addMask(winInfo, PRIMARY_MONITOR_MASK | X_CENTERED_MASK);
+        removeMask(winInfo, ABOVE_MASK);
+        setTilingOverrideEnabled(winInfo, 1 | 4 | 32);
+        setTilingOverride(winInfo, (Rect) {-140, 0, 520, 0});
+    }
+    else if(matchesClass(winInfo, "dzen2")) {
+        addMask(winInfo, PRIMARY_MONITOR_MASK | BELOW_MASK);
+        removeMask(winInfo, ABOVE_MASK);
+        setTilingOverrideEnabled(winInfo, 3);
+        lowerWindowInfo(winInfo, 0);
+    }
+    if(matchesClass(winInfo, "remote")) {
+        addMask(winInfo, PRIMARY_MONITOR_MASK | ABOVE_MASK | Y_MAXIMIZED_MASK);
+        setTilingOverrideEnabled(winInfo, 3);
+        moveToWorkspace(winInfo, getActiveWorkspaceIndex());
+    }
+    if(matchesClass(winInfo, "tabbed")) {
+        addMask(winInfo, INPUT_MASK);
+    }
+    if(matchesClass(winInfo, "vimb")) {
+        strcpy(winInfo->role, "browser");
+        //if(winInfo->getCreationTime())
+        //    winInfo->moveToWorkspace(3);
+    }
+    if(matchesClass(winInfo, "st-256color")) {
+        strcpy(winInfo->role, "terminal");
+        setWindowClass(winInfo->id, "st", "st");
+        loadWindowProperties(winInfo);
+    }
+    if(!hasMask(winInfo, INPUT_MASK) && winInfo->type == ewmh->_NET_WM_WINDOW_TYPE_NORMAL) {
+        addMask(winInfo, INPUT_MASK);
+    }
+    if(winInfo->dock) {
+        removeMask(winInfo, ABOVE_MASK);
+        setTilingOverrideEnabled(winInfo, 16);
+    }
+    if(winInfo->type == ewmh->_NET_WM_WINDOW_TYPE_NOTIFICATION)
+        addMask(winInfo, ABOVE_MASK);
+}
+
+void onXConnectionStart() {
+    loadCustomAtoms();
+}
+
+void addNormalBindings();
+void enableGestureBindings();
+void loadSettings() {
+    DEFAULT_BORDER_COLOR = 0x00FF00;
+    LD_PRELOAD_INJECTION = 1;
+    // IDLE_TIMEOUT = 100;
+    ALLOW_SETTING_UNSYNCED_MASKS = 1;
+    spawnPipe("status-bar -title-name 'wm-status'", REDIRECT_CHILD_INPUT_ONLY);
+    loadNormalSettings();
+    addNormalBindings();
+    setWorkspaceName(3, "VimB");
+    setWorkspaceName(4, "St");
+    setWorkspaceName(6, "7Gaming");
+    addWorkspaceMask(getWorkspace(6), FLOATING_MASK);
+    addEvent(X_CONNECTION, USER_EVENT(onXConnectionStart, HIGH_PRIORITY));
+    /*
+    getEventRules(CLIENT_MAP_ALLOW).add({
+        +[](WindowInfo * winInfo) {
+            if(matchesTitle(winInfo, "Steam - News"))
+                winInfo->moveToWorkspace(8);
+        }, "HideSteamNews"});
+    */
+    addEvent(CLIENT_MAP_ALLOW, USER_EVENT(handleSteamGame));
+    addEvent(CLIENT_MAP_ALLOW, USER_EVENT(configureWindowsByName));
+    //getEventRules(CLIENT_MAP_ALLOW).add([](WindowInfo*winInfo){addMask(winInfo,INPUT_MASK);}, HIGH_PRIORITY);
+    //getEventRules(CLIENT_MAP_ALLOW).add(USER_EVENT_P(configureWindows, LOW_PRIORITY));
+    addSuggestedRules();
+    addInsertWindowsAtPositionRule(BEFORE_FOCUSED);
+    // Extensions
+    addAutoDetectDockPosition();
+    addAutoMPXRules();
+    addCloneRules();
+    addEWMHRules();
+    addFloatRule();
+    addMoveNonTileableWindowsToWorkspaceBounds();
+    addPrintStatusRule();
+    addResumeContainerRules();
+    addResumeCustomStateRules();
+    addStickyPrimaryMonitorRule();
+    /* TODO
+    addAutoFocusRule();
+    addDesktopRule();
+    addEWMHRules();
+    addFloatRule();
+    addIgnoreInputOnlyWindowsRule();
+    addIgnoreNonTopLevelWindowsRule();
+    addKeepTransientsOnTopRule();
+    addMoveNonTileableWindowsToWorkspaceBounds();
+    addPrintStatusRule();
+    addStickyPrimaryMonitorRule();
+    */
+    // gestures
+    enableGestureBindings();
+}
