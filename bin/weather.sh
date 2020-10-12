793a4bc1e0a859a5f6122f821b9f64ddb1eaa43b
Arthur Williams <taaparthur@gmail.com>
2020-10-12 00:32:52 -0700

    Better caching for weather

--- bin/weather.sh
+++ bin/weather.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
 
-main () {
-
     file="/tmp/.weather"
-
+checkCache() {
     if [[ -f "$file" && ($(date -r "$file" +%s) > $(date -d 'now - 1 hour' +%s)) ]]; then
         cat $file
         exit 0
     fi
+}
+
+main () {
+    checkCache
 
     if ! ping -q -c 1 -W 1 google.com &>/dev/null; then
         touch $file
         echo $(cat $file)'*'
         exit 1
     fi
-    tempC=$(curl -s 'wttr.in?m&format=%t&lang=uk')
-    tempF=$(curl -s 'wttr.in?u&format=%t&lang=uk')
-    if [ "$?" -ne 0 ] || [ -z "$tempC" ] || [[ "$tempC" == "Unknown"* ]] ; then
+    temp=$(curl -s 'wttr.in?m&format=%t&lang=uk' & curl -s 'wttr.in?u&format=%t&lang=uk' )
+    if [ "$?" -ne 0 ] || [ -z "$temp" ] || [[ "$temp" == "Unknown"* ]] ; then
         touch $file
         echo $(cat $file)'*'
     else
-        output=$(echo "$tempC;$tempF"|sed -r 's/[^CF0-9; ]//g' )
-        echo "$output" >$file
-        echo "$output"
+        output=$(echo "$temp" | sed "s/[^CF0-9]//g" )
+        echo $output >$file
+        echo $output
     fi
 
 }
+if [  "$1" == "-c" ]; then
+    checkCache
+    exit
+fi
 
 main
