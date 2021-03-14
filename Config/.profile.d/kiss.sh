0645813988e7612a7fe39b0584cd79f10d288cc8
Arthur Williams <taaparthur@gmail.com>
2021-03-14 15:40:19 -0500

    Prepend instead of overriding KISS_PATH

--- Config/.profile.d/kiss.sh
+++ Config/.profile.d/kiss.sh
@@ -2,9 +2,6 @@
 
 # Kiss linux specific vars
 REPO_HOME=~/Forks/mykiss-repo
-export KISS_PATH=$REPO_HOME/personal:$REPO_HOME/forks:$REPO_HOME/prebuilt:$REPO_HOME/ban:$REPO_HOME/meta_packages:$KISS_PATH_ORIGINAL
-KISS_PATH=$KISS_PATH:$REPO_HOME/extra:$REPO_HOME/python
-KISS_PATH=$KISS_PATH:$REPO_HOME/fallback
-KISS_PATH=$KISS_PATH:/etc/repo/kiss-all/audio
-KISS_PATH=$KISS_PATH:$REPO_HOME/nonfree
+KISS_PATH_PREFIX=$REPO_HOME/personal:$REPO_HOME/forks:$REPO_HOME/prebuilt:$REPO_HOME/ban:$REPO_HOME/meta_packages
+export KISS_PATH=$KISS_PATH_PREFIX:$REPO_HOME/extra:$REPO_HOME/python:$REPO_HOME/nonfree:$REPO_HOME/fallback:$KISS_PATH
 export KISS_FALLBACK_REPO=~/Forks/mykiss-repo/fallback
