c2babd2c0c0997a1bb1130eec878083587c67713
Arthur Williams <taaparthur@gmail.com>
2021-04-15 19:40:18 -0700

    Added ringd script

---
+++ Config/.config/ringd/99-default.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+vibrator -c1 -d0 -l 1000 &
+if notify-send -r "phone-$NUMBER" "Call from $(contacts get-name "$NUMBER" || echo "$NUMBER") $ALPHA"; then
+    call -a
+else
+    call -h
+fi
