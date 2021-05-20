d6b8fdc7a9e987d6a421f2d2f2f2b9439e4f6227
Arthur Williams <taaparthur@gmail.com>
2021-05-20 00:05:41 -0700

    [bin/link_dot_files] Make more portable
    Resolve shellcheck errors
    Don't use non standard diff flags
    Auto remove directly when force linking

--- bin/link_dot_files.sh
+++ bin/link_dot_files.sh
@@ -4,33 +4,34 @@ safeLn() {
     srcFile=$1
     target=$2
     linkTarget=$2
-    [ -e $srcFile ]
-    if [ -d $target ]; then
-        target="$target/$(basename $srcFile)"
+    [ -e "$srcFile" ]
+    if [ -d "$target" ]; then
+        target="$target/$(basename "$srcFile")"
     fi
-    if [ ! -e $target ] || diff -q $srcFile $target; then
-        ln -sf $srcFile $linkTarget
+    if [ ! -e "$target" ] || diff -q "$srcFile" "$target"; then
+        ln -sf "$srcFile" "$linkTarget"
     else
-        diff -Ny --suppress-common-lines $srcFile $target || true
-        echo "Override  and link $target to $srcFile ? Y/n"
+        diff -Nu "$srcFile" "$target" || true
+        echo "Override and link $target to $srcFile ? Y/n"
         read -r ans
         if [ "$ans" = Y ] || [ "$ans" = y ]; then
-            ln -sf $srcFile $linkTarget
+            [ -d "$target" ] && rmdir "$target"
+            ln -sf "$srcFile" "$linkTarget"
         fi
     fi
 }
 
 SYSTEM_CONFIG_DIR=${SYSTEM_CONFIG_DIR:-~/SystemConfig}
-cd $SYSTEM_CONFIG_DIR/Config
+cd "$SYSTEM_CONFIG_DIR/Config"
 for dotFile in .*; do
-    if [ $dotFile = "." ] || [ $dotFile = ".." ]; then
+    if [ "$dotFile" = "." ] || [ "$dotFile" = ".." ]; then
         continue
-    elif [ -d $dotFile ]; then
-        mkdir -p ~/$dotFile
+    elif [ -d "$dotFile" ]; then
+        mkdir -p "$HOME/$dotFile"
         for config in "$dotFile"/*; do
-            [ -e $config ] && safeLn $PWD/$config ~/$dotFile/
+            [ -e "$config" ] && safeLn "$PWD/$config" "$HOME/$dotFile/"
         done
     else
-        safeLn $PWD/$dotFile ~/$dotFile
+        safeLn "$PWD/$dotFile" "$HOME/$dotFile"
     fi
 done
