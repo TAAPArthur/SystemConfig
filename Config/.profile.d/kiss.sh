5c6d86dfb03b12c621b7c3c48c2877f13ee6f167
Arthur Williams <taaparthur@disroot.org>
2021-12-11 15:46:56 -0600

    profile.d/kiss.sh: Use default fallback repo path

--- Config/.profile.d/kiss.sh
+++ Config/.profile.d/kiss.sh
@@ -2,5 +2,4 @@
 # shellcheck disable=SC1091
 
 MY_REPO_DIR=~/Forks/mykiss-repo
-[ -e "$MY_REPO_DIR" ] && [ -r /etc/profile.d/00-kiss.sh ] && . /etc/profile.d/00-kiss.sh
-export KISS_FALLBACK_REPO=~/Forks/mykiss-repo/fallback
+[ -d "$MY_REPO_DIR" ] && [ -r /etc/profile.d/00-kiss.sh ] && . /etc/profile.d/00-kiss.sh
