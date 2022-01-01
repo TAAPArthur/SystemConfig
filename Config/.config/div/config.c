1177430afe7222e757fe5ba384059cd40ce3235e
Arthur Williams <taaparthur@disroot.org>
2021-12-31 19:40:17 -0800

    div: misc update
    Sort imports
    Added mouse navigation;
    Auto quit on last page with mouse
    Improved the large page detection
    Used system command instead of rolling my own
    Also handle malicious filenames when copying

--- Config/.config/div/config.c
+++ Config/.config/div/config.c
@@ -1,32 +1,19 @@
 #include <X11/X.h>
 #include <X11/keysym.h>
-#include <xcb/xcb.h>
 #include <div/div.h>
 #include <div/functions.h>
 #include <div/image_view.h>
 #include <div/x.h>
 #include <img_loader/img_loader.h>
 #include <stdio.h>
-#include <sys/wait.h>
 #include <string.h>
+#include <sys/wait.h>
 #include <unistd.h>
+#include <xcb/xcb.h>
 
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
 #include <stb/stb_image_resize.h>
 
-
-int spawn(const char* command) {
-    int pid = fork();
-    if(pid == 0) {
-        const char* const args[] = {"/bin/sh", "-c", command, NULL};
-        execv(args[0], (char* const*)args);
-        exit(1);
-    }
-    else if(pid < 0) exit(1);
-    int status = 0;
-    waitpid(pid, &status, 0);
-    return WIFEXITED(status) ? WEXITSTATUS(status) : WIFSIGNALED(status) ? WTERMSIG(status) : -1;
-}
 
 void windowTitle() {
     static char buffer[255];
@@ -49,16 +36,28 @@ void sort(int arg) {
 
 extern xcb_generic_event_t* event;
 void printSelectedImage() {
+    static char buffer[] = "printf %s \"$FILENAME\" | xsel -b";
     xcb_button_press_event_t* bevent = (xcb_button_press_event_t*)event;
     for (int i = 0; i < getNumActiveImages(); i++) {
         if(image_holders[i].geometry.x < bevent->event_x && bevent->event_x < image_holders[i].geometry.x + image_holders[i].geometry.width )
             if(image_holders[i].geometry.y < bevent->event_y && bevent->event_y < image_holders[i].geometry.y + image_holders[i].geometry.height ) {
                 printf("%s\n", image_holders[i].name);
-                static char buffer[255];
-                snprintf(buffer, sizeof(buffer) - 1, "printf %s | xsel ", image_holders[i].name);
-                spawn(buffer);
+                setenv("FILE_NAME", image_holders[i].name, 1);
+                system(buffer);
                 return;
             }
+    }
+}
+
+void buttonNavigate() {
+    xcb_button_press_event_t* bevent = (xcb_button_press_event_t*)event;
+    int index = state.file_index;
+    if(bevent->event_x < state.win_width / 3 )
+        next_page(state.right_to_left ? 1 : -1);
+    if(bevent->event_x > state.win_width * 2 / 3)
+        next_page(!state.right_to_left ? 1 : -1);
+    if(state.file_index && index == state.file_index) {
+        exit(0);
     }
 }
 
@@ -69,7 +68,8 @@ Binding user_bindings[] = {
     {ShiftMask, XK_n, sort, -IMG_SORT_NAME},
     {ShiftMask, XK_a, sort, IMG_SORT_MOD},
     {0, XK_a, sort, -IMG_SORT_MOD},
-    {0, Button1, printSelectedImage, .type=-1},
+    {0, Button1, printSelectedImage, .type=XCB_BUTTON_PRESS},
+    {0, Button1, buttonNavigate, .type=XCB_BUTTON_RELEASE},
     {0}
 };
 
@@ -91,7 +91,7 @@ void customOpenImages() {
     defaultOpenImages();
     if(getNumActiveImages() == 2) {
         for (int i = 0; i < getNumActiveImages(); i++) {
-            if(image_holders[i].image_data && image_holders[i].image_width >= 2048) {
+            if(image_holders[i].image_data && image_holders[i].image_width >= 1024 + 256 && image_holders[i].image_width != image_holders[!i].image_width) {
                 largePage = true;
                 state.cols = 1;
             }
@@ -99,7 +99,7 @@ void customOpenImages() {
     }
 }
 void onStartup(){
-    state.xevent_mask |= XCB_EVENT_MASK_BUTTON_PRESS ;
+    state.xevent_mask |= XCB_EVENT_MASK_BUTTON_PRESS | XCB_EVENT_MASK_BUTTON_RELEASE;
     events[SET_TITLE] = windowTitle;
     events[XCB_BUTTON_RELEASE] = events[XCB_KEY_PRESS];
     events[XCB_BUTTON_PRESS] = events[XCB_KEY_PRESS];
