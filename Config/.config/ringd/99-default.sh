98b657b9433fcb2b95dde27c91ecca9edad62ddc
Arthur Williams <taaparthur@disroot.org>
2022-01-01 13:02:03 -0600

    ringd: use exec for final command

--- Config/.config/ringd/99-default.sh
+++ Config/.config/ringd/99-default.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 vibrator -c1 -d0 -l 1000 &
-if notify-send -r "phone-$NUMBER" "Call from $(contacts get-name "$NUMBER" || echo "$NUMBER") $ALPHA"; then
-    call -a
+if notify-send -c call -r "phone-$NUMBER" "Call from $(contacts get-name "$NUMBER" || echo "$NUMBER") $ALPHA"; then
+    exec call -a
 else
-    [ "$?" -eq 2 ] && call -h
+    [ "$?" -eq 2 ] && exec call -h
 fi
