339057e0d79d80c5204a1b1a842d965a10630485
Arthur Williams <taaparthur@disroot.org>
2022-05-28 01:44:53 -0700

    history_cleanup: rewrite
    Simplified how shell history is cleaned on startup

--- Config/.profile.d/history_cleanup.sh
+++ Config/.profile.d/history_cleanup.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-if [ -n "$HISTFILE" ] && [ -n "$HISTSIZE" ]; then
-    [ "$(wc -l "$HISTFILE" | cut -d" " -f1)" -gt $((HISTSIZE/2)) ] && history_cleanup.sh "$HISTFILE"
-fi
+history_cleanup.sh "$HISTFILE"
--- bin/history_cleanup.sh
+++ bin/history_cleanup.sh
@@ -4,7 +4,14 @@ SIZE="${2:-1000}"
 [ -n "$HIST_CLEAN" ] || HIST_CLEAN=1 exec flock -n "$FILE" "$0" "$@"
 DIR="$(mktemp -d)"
 trap 'rm -r $DIR' EXIT
+
 cd "$DIR"
-tail -n +"$SIZE" "$FILE" | grep -v -e "^#" -e "^kiss" -e "^ls" -e "^fg" | sort -u > temp
-tail -n "$SIZE" "$FILE" >> temp
+
+grep -v -x -e ls -e fg -e F -e fg -e bk "$FILE" | uniq > temp
+
+if [ "$(wc -l "temp" | cut -d" " -f1)" -gt $((HISTSIZE/2)) ]; then
+    tail -n +"$SIZE" "temp" | sort -u > temp2
+    tail -n "$SIZE" "temp" >> temp2
+    mv temp2 temp
+fi
 mv temp "$FILE"
