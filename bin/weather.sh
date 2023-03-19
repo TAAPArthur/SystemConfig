ea7b7cf3fb6a9de42b7a73fc3bf6bd121b0a652a
Arthur Williams <taaparthur@disroot.org>
2023-03-19 02:10:12 -0700

    weather.sh: added a timeout

--- bin/weather.sh
+++ bin/weather.sh
@@ -1,36 +1,33 @@
-#!/bin/sh -e
+#!/bin/sh -ex
 
 file="/tmp/.weather"
 tmpfile="$file.tmp"
 timestampFile="$file.ts"
 checkCache() {
-    if [ -f "$file" ] && [ -f "$timestampFile" ] && [ "$(date -u +%s)" -lt $(($(cat $timestampFile) + 3600)) ]; then
+    if [ -s "$file" ] && [ -f "$timestampFile" ] && [ "$(date -u +%s)" -lt $(($(cat $timestampFile) + 3600)) ]; then
         cat $file
         exit 0
     fi
 }
-updateTimeStamp() {
-    date -u +%s > $timestampFile
-}
 
-main () {
-    checkCache
-
-    if { curl -s 'wttr.in?m&format=%t&lang=uk' && curl -s 'wttr.in?u&format=%t&lang=uk'; } > $tmpfile && grep "F" "$tmpfile" && grep "C" "$tmpfile"; then
-        sed "s/[^CF0-9]//g" $tmpfile | tee $file
-        rm $tmpfile
-        updateTimeStamp
-    else
-        rm $tmpfile
-        [ -r "$file" ] || cat $file
-        printf '*'
-        updateTimeStamp
-        exit 1
-    fi
-}
+checkCache
 if [ "$1" = "-c" ]; then
-    checkCache
     exit
 fi
 
-main
+[ -n "$WEATHER_LOCK" ] || WEATHER_LOCK=1 exec flock -n "$file" "$0"
+
+updateTimeStamp() {
+    date -u +%s > "$timestampFile"
+}
+trap 'rm "$tmpfile"' EXIT
+
+if curl -m 2 -s 'wttr.in?{m,u}&format=%t&lang=uk' > "$tmpfile" && grep -q "F" "$tmpfile" && grep -q "C" "$tmpfile"; then
+    sed "s/[^CF0-9]//g" "$tmpfile" | tee $file
+    updateTimeStamp
+else
+    [ -s "$file" ] && cat "$file"
+    printf '*'
+    updateTimeStamp
+    exit 1
+fi
