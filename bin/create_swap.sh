30838a82e1b911c2f1b724d22a8913075d277443
Arthur Williams <taaparthur@disroot.org>
2023-03-19 02:09:09 -0700

    create_swap: made more versitle

--- bin/create_swap.sh
+++ bin/create_swap.sh
@@ -1,18 +1,18 @@
-#!/bin/sh -e
+#!/bin/sh -ex
 
-SWAP_FILE=${SWAP_FILE:-$KISS_ROOT/.swapfile}
-if ! grep -q swap "$KISS_ROOT/etc/fstab"; then
-    if [ ! -e "$SWAP_FILE" ]; then
-        # Create a 4GB swap file by default
-        SWAP_FILE_SIZE=${SWAP_FILE_SIZE:-4096}
-        dd if=/dev/zero of="$KISS_ROOT/$SWAP_FILE" bs=1M count="$SWAP_FILE_SIZE" status=progress
-        chmod 600 "$KISS_ROOT/$SWAP_FILE"
-        mkswap "$KISS_ROOT/$SWAP_FILE"
-        swapon "$KISS_ROOT/$SWAP_FILE"
-        echo "$SWAP_FILE none swap defaults 0 0" >> "$KISS_ROOT/etc/fstab"
-    else
-        echo "Swap file $SWAP_FILE exists"
-    fi
+SWAP_FILE=${SWAP_FILE:-$PKGMAN_ROOT/.swapfile}
+if [ ! -e "$SWAP_FILE" ]; then
+    # Create a 4GB swap file by default
+    SWAP_FILE_SIZE=${SWAP_FILE_SIZE:-4096}
+    dd if=/dev/zero of="$PKGMAN_ROOT/$SWAP_FILE" bs=$((1<<20)) count="$SWAP_FILE_SIZE"
+    chmod 600 "$PKGMAN_ROOT/$SWAP_FILE"
+    mkswap "$PKGMAN_ROOT/$SWAP_FILE"
+    swapon "$PKGMAN_ROOT/$SWAP_FILE"
+else
+    echo "Swap file $SWAP_FILE exists"
+fi
+if ! grep -q swap "$PKGMAN_ROOT/etc/fstab"; then
+    echo "$SWAP_FILE none swap defaults 0 0" >> "$PKGMAN_ROOT/etc/fstab"
 else
     echo "Swap file is already mentioned in /etc/fstab"
 fi
